39c0835802615711cddf46506039b1bc15403421
Li Xing <lxedge@sina.com>
2021-07-22 16:19:40 +0800

    update init.el

--- init.el
+++ init.el
@@ -1,24 +1,24 @@
 ;;; -*- lexical-binding: t -*-
 
 (require 'init-straight)
-(require 'init-settings)
-(require 'init-window)
-(require 'init-sidebar)
-(require 'init-themes)
-(require 'init-grep)
-(require 'init-isearch)
-(require 'init-ivy)
-(require 'init-exec-path-from-shell)
-
-(require 'init-company)
-(require 'init-paredit)
-
-(require 'init-projectile)
 (require 'init-version-control)
 
-(require 'init-elixir)
+(require 'init-window)
+(require 'init-themes)
+(require 'init-sidebar)
 
+(require 'init-grep)
+(require 'init-ivy)
+(require 'init-anzu)
+
+;; (require 'init-exec-path-from-shell)
+(require 'init-company)
+(require 'init-paredit)
+(require 'init-projectile)
+
+;; not used recently
 ;; (require 'init-common-lisp)
 
+;; (require 'init-elixir)
 
 (provide 'init)
