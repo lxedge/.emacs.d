9ba14d885db621e8a63538381595e88af8dc4d29
Li Xing <lxedge@sina.com>
2021-07-22 15:45:27 +0800

    format init-projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,5 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
+;; https://docs.projectile.mx/projectile/usage.html
+
 (use-package projectile
   :init
   (add-hook 'after-init-hook 'projectile-mode)
@@ -11,21 +13,21 @@
 (use-package ibuffer-projectile
   :init
   (add-hook 'ibuffer-hook
-	    (lambda ()
-	      (ibuffer-projectile-set-filter-groups)
-	      (unless (eq ibuffer-sorting-mode 'alphabetic)
-		(ibuffer-do-sort-by-alphabetic))))
+            (lambda ()
+              (ibuffer-projectile-set-filter-groups)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic))))
 
   :config
   (setq ibuffer-formats
-	'((mark modified read-only " "
-		(name 18 18 :left :elide)
-		" "
-		(size 9 -1 :right)
-		" "
-		(mode 16 16 :left :elide)
-		" "
-		project-relative-file))))
+        '((mark modified read-only " "
+                (name 18 18 :left :elide)
+                " "
+                (size 9 -1 :right)
+                " "
+                (mode 16 16 :left :elide)
+                " "
+                project-relative-file))))
 
 
 (provide 'init-projectile)
