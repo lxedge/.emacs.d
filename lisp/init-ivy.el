685e87da736ebcb343993b5084f802b66981f08b
Li Xing <lxedge@sina.com>
2021-07-22 13:59:23 +0800

    add some global kdb when init ivy

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -1,20 +1,47 @@
 ;;; -*- lexical-binding: t -*-
 
+;; https://oremacs.com/swiper/
+
 (use-package ivy
   :init
   (add-hook 'after-init-hook 'ivy-mode)
+  ;; (global-set-key (kbd "C-s") 'swiper-isearch)
+  ;; (global-set-key (kbd "C-c v") 'ivy-push-view)
+  ;; (global-set-key (kbd "C-c V") 'ivy-pop-view)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+  (global-set-key (kbd "M-y") 'counsel-yank-pop)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "<f2> j") 'counsel-set-variable)
+
+  ;; (global-set-key (kbd "C-c c") 'counsel-compile)
+  ;; (global-set-key (kbd "C-c g") 'counsel-git)
+  ;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c L") 'counsel-git-log)
+  (global-set-key (kbd "C-c k") 'counsel-rg)
+  (global-set-key (kbd "C-c m") 'counsel-linux-app)
+  (global-set-key (kbd "C-c n") 'counsel-fzf)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-c J") 'counsel-file-jump)
+  ;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  ;; (global-set-key (kbd "C-c w") 'counsel-wmctrl)
 
   :config
   (setq-default ivy-use-virtual-buffers t
-		ivy-virtual-abbreviate 'fullpath
-		ivy-count-format ""
-		projectile-completion-system 'ivy
-		ivy-magic-tilde nil
-		ivy-dynamic-exhibit-delay-ms 150
-		ivy-use-selectable-prompt t
-		ivy-initial-inputs-alist
-		'((Man-completion-table . "^")
-		  (woman . "^")))
+                ivy-virtual-abbreviate 'fullpath
+                ivy-count-format ""
+                projectile-completion-system 'ivy
+                ivy-magic-tilde nil
+                ivy-dynamic-exhibit-delay-ms 150
+                ivy-use-selectable-prompt t
+                ivy-initial-inputs-alist
+                '((Man-completion-table . "^")
+                  (woman . "^")))
   (define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done)
   (define-key ivy-minibuffer-map (kbd "<up>") #'ivy-previous-line-or-history)
   (define-key ivy-occur-mode-map (kbd "C-c C-q") #'ivy-wgrep-change-to-wgrep-mode)
