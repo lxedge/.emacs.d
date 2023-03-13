e7e1eaa60bc585e3b729a6e154f2e6b9a2e9deaf
Li Xing <lxedge@sina.com>
2023-03-13 09:10:09 +0800

    remove paredit

--- init.el
+++ init.el
@@ -2,31 +2,28 @@
 
 ;; 使用 straight
 (require 'init-straight)
+;; 感觉和 symbol-overlay 差不多，先去除
+;; (require 'init-anzu)
+;; (require 'init-grep)
+;; (require 'init-exec-path-from-shell)
 ;; diff-hl 侧边栏添加 vcs 状态
 (require 'init-version-control)
-
 ;; UI 相关配置
 (require 'init-ui)
-
 ;; 编辑功能相关
 (require 'init-edit)
-
-(require 'init-grep)
 (require 'init-ivy)
-(require 'init-anzu)
 
-(require 'init-exec-path-from-shell)
 (require 'init-company)
-(require 'init-paredit)
 (require 'init-projectile)
 
 ;; not used recently
 ;; (require 'init-common-lisp)
+;; (require 'init-python)
+;; (require 'init-golang)
 
 (require 'init-elixir)
 (require 'init-clojure)
 (require 'init-rust)
-;; (require 'init-python)
-;; (require 'init-golang)
 
 (provide 'init)
