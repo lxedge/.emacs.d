19f196bf8684bbecce88ead58b007321d5bab8d3
Li Xing <lxedge@sina.com>
2023-03-15 02:06:04 +0800

    change init-rust

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,40 +1,36 @@
 ;;; -*- lexical-binding: t -*-
 
 (use-package rust-mode
+  :ensure t
+  :mode (("\\.rs\\'" . rust-mode))
   :init
-  (add-hook 'rust-mode-hook
-            (lambda () (setq indent-tabs-mode nil)))
-  (add-hook 'rust-mode-hook #'lsp)
+  (add-hook 'rust-mode-hook (lambda () (setq indent-tabs-mode nil)))
+  (add-hook 'rust-mode-hook (lambda () (prettify-symbols-mode)))
+  (add-hook 'rust-mode-hook 'eglot-ensure)
+  (add-hook 'rust-mode-hook 'lsp-deferred)
   (add-hook 'rust-mode-hook 'cargo-minor-mode)
-  (setq rust-format-on-save t))
 
-(use-package lsp-mode
-  :ensure t
-  :bind-keymap ("C-c l" . lsp-command-map)
-  :config (lsp-enable-which-key-integration t))
-
-(use-package company
-  :ensure t
-  :hook ((emacs-lisp-mode . (lambda ()
-                              (setq-local company-backends '(company-elisp))))
-         (emacs-lisp-mode . company-mode))
   :config
-  (company-keymap--unbind-quick-access company-active-map)
-  (company-tng-configure-default)
-  (setq company-idle-delay 0.1
-        company-minimum-prefix-length 1))
+  (setq rust-format-on-save t)
+  (setq rustic-lsp-server 'rust-analyzer))
 
-(use-package flycheck
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :bind-keymap ("C-c l" . lsp-command-map)
+;;   :config (lsp-enable-which-key-integration t))
+
+(use-package tree-sitter-langs
   :ensure t)
 
-;; (use-package tree-sitter
-;;   :config
-;;   (require 'tree-sitter-langs)
-;;   (global-tree-sitter-mode)
-;;   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(use-package tree-sitter
+  :ensure t
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
-(use-package rustic)
-(setq rustic-lsp-server 'rust-analyzer)
+(use-package rustic
+  :config (setq rustic-lsp-client 'eglot))
 
 (use-package cargo-mode
   :config
